118ca572844afbbae28114ab69a274ffa52dfb59
Dmytro Benko <dimcot8@MacBook-Pro.local>
2023-06-17 21:29:42 -0400

    Assignment 1 commit

--- src/api.js
+++ src/api.js
@@ -1,26 +1,71 @@
 // src/api.js
 
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:1234';
+const apiUrl = process.env.API_URL || 'http://ec2-user@ec2-184-72-71-226.compute-1.amazonaws.com:8080';
 
 /**
  * Given an authenticated user, request all fragments for this user from the
  * fragments microservice (currently only running locally). We expect a user
  * to have an `idToken` attached, so we can send that along with the request.
  */
-export async function getUserFragments(user) {
+export async function getUserFragments(user, expand) {
   console.log('Requesting user fragments data...');
   try {
-    const res = await fetch(`${apiUrl}/v1/fragments`, {
+    let url = `${apiUrl}/v1/fragments`;
+    if (expand) url += '?expand=1';
+
+    const res = await fetch(url, {
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
     });
+
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
     const data = await res.json();
     console.log('Got user fragments data', { data });
   } catch (err) {
-    // console.error('Unable to call GET /v1/fragment', { err });
+    console.error('Unable to call GET /v1/fragments', { err });
+  }
+}
+
+
+
+
+export async function getFragmentInfo(user, id) {
+  console.log('Requesting fragment info...');
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      headers: user.authorizationHeaders(),
+    });
+    if (res.status === 404) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Got user fragment info', { data});
+  } catch (err) {
+    console.error(`Unable to call GET /v1/fragments/${id}`, { err });
+  }
+}
+
+
+export async function postFragment(user, fragment, type) {
+  console.log('Posting user fragment data...');
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+        'Content-Type': type,
+      },
+      body: fragment,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    console.log('User fragment data posted', { fragment });
+  } catch (err) {
+    console.error('Unable to call POST /v1/fragment', { err });
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,19 +1,36 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-import { getUserFragments } from './api';
+import { getFragmentData, getUserFragments } from './api';
+import {
+  getUserFragments,
+  postFragment,
+  getFragmentInfo
+
+} from './api';
 
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
+  const postSection = document.querySelector('#post-section');
+  const postBtn = document.querySelector('#post-btn');
+  const getSection = document.querySelector('#get-section');
+  const fragmentType = document.querySelector('#fragmentType');
+  const fileInput = document.querySelector('#file-input');
+  const getInfoBtn = document.querySelector('#get-fragment-btn');
+  const getFragmentsBtn = document.querySelector('#get-fragments-btn');
+  const fragmentId = document.querySelector('#fragment-id');
+  const fragmentInput = document.querySelector('#inputfragment');
+
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
     // Sign-in via the Amazon Cognito Hosted UI (requires redirects), see:
     // https://docs.amplify.aws/lib/auth/advanced/q/platform/js/#identity-pool-federation
     Auth.federatedSignIn();
+    
   };
   logoutBtn.onclick = () => {
     // Sign-out of the Amazon Cognito Hosted UI (requires redirects), see:
@@ -23,9 +40,11 @@ async function init() {
 
   // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
+  
   if (!user) {
     // Disable the Logout button
     logoutBtn.disabled = true;
+
     return;
   }
 
@@ -40,7 +59,47 @@ async function init() {
 
   // Disable the Login button
   loginBtn.disabled = true;
+  postSection.hidden = false;
+  getSection.hidden = false;
+
   getUserFragments(user);
+
+  getFragmentsBtn.addEventListener('click', () => {
+    getUserFragments(user, true);
+  });
+
+  postBtn.onclick = () => {
+    if (fragmentType.value === '') {
+      alert('Please select a type');
+      return;
+    }
+  
+    if (fileInput.files.length != 0) {
+      const file = fileInput.files[0];
+      const name = file.name;
+      var ext = name.substr(name.lastIndexOf('.') + 1, name.length);
+  
+  
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const fileData = e.target.result;
+        postFragment(user, fileData, fragmentType.value);
+      };
+      reader.readAsText(file);
+  
+      return;
+    }
+  
+    if (fragmentInput === '') alert('Please enter a fragment');
+    else postFragment(user, fragmentInput, fragmentType);
+  };
+
+  getInfoBtn.addEventListener('click', () => {
+    if (fragmentId.value === '' || fragmentId.value === null) alert("Fragment ID can't be blank.");
+    else getFragmentInfo(user, fragmentId.value);
+  });
+
+
 
 }
 
